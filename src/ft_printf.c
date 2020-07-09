32c5cde02a1ab4d7faba236c2d762276f0f962a4
Chanda Sparrow <chsparro@va-h5.kzn.21-school.ru>
2020-07-09 17:26:04 +0300

    Вывод строки, файл .gitignore

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,8 +6,25 @@
 /*   By: chsparro <chsparro@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 16:26:36 by chsparro          #+#    #+#             */
-/*   Updated: 2020/07/09 16:26:43 by chsparro         ###   ########.fr       */
+/*   Updated: 2020/07/09 17:23:08 by chsparro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <unistd.h>
+void ft_printf(char *format)
+{
+	int i;
 
+	i = 0;
+	while (!(format[i] == '\0'))
+	{
+		write(1, &format[i], 1);
+		i++;
+	}
+}
+int main(void)
+{
+	ft_printf("123");
+	return (0);
+}
